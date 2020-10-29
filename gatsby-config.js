aed2d4e67e668e8a17b0f2ffbc9641e624572854
marcoSven <me@marcosven.com>
2020-10-28 18:21:51 -0700

    gatsby-rehype-inline-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,9 @@ module.exports = {
                     {
                         resolve: `gatsby-rehype-prismjs`,
                     },
+                    {
+                        resolve: `gatsby-rehype-inline-images`,
+                    },
                 ],
             },
         },
